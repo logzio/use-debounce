1337fdcf0be1e6186398556d14ff011a614c3f0b
Eden Turgeman <eden.turgeman@logz.io>
2019-08-11 14:11:30 +0300

    feat(react-hook): Initial version of the debounce hook.

---
+++ src/debounce.hook.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+// Inspiration from https://dev.to/gabe_ragland/debouncing-with-react-hooks-jci
+/**
+ *
+ * @param value - The value to be debounced. When changes to this value stop for the set amount of time the operation will be executed.
+ * @param operation - The operation to execute after the delay.
+ * @param delay - The time in ms to wait for the operation execution.
+ * @param params - Optional params to be passed to the callback operation.
+ */
+export const useDebounce = (value, operation, delay = 400, ...params) => {
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            operation(value, ...params);
+        }, delay);
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value]);
+};
